ca5432b7c1408898f1e0aa8a98162c46c2a111d3
Alex <aldmpr@gmail.com>
2022-06-29 14:34:38 +0200

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const answers = [];
+
+answers[0] = prompt("What is your Name", "");
+answers[1] = prompt("What is your Surname", "");
+answers[2] = prompt("What is your Age", "");
+
+console.log(typeof(answers));
